2aebe220e74e01af33f4565f92f9ef37ab92e665
Bretton Wade <bretton.wade@gmail.com>
2016-05-10 10:05:49 -0400

    - BadVersionException is public - Adding reference material for an XML scanner I wrote years back in C++ - thinking of   using a Java version of that

--- src/main/java/com/brettonw/bag/BadVersionException.java
+++ src/main/java/com/brettonw/bag/BadVersionException.java
@@ -1,6 +1,6 @@
 package com.brettonw.bag;
 
-class BadVersionException extends RuntimeException {
+public class BadVersionException extends RuntimeException {
     public BadVersionException (String got, @SuppressWarnings ("SameParameterValue") String expected) {
         super ("Incorrect version (got: " + got + ", expected: " + expected + ")");
     }
