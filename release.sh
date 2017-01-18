6e5d0d3a5a0c4db813342a85e079fb45dfa5b16d
Bretton Wade <bretton.wade@gmail.com>
2017-01-18 17:27:55 -0500

    updating release

--- release.sh
+++ release.sh
@@ -2,7 +2,6 @@
 
 go --clean package && git add --all . && git commit && git push && \
 git checkout master && git pull && git merge develop && git push && \
-go --clean package && git add --all . && git commit && git push && \
 go release &&
 git checkout develop && git pull && git merge master && git push && \
 go --clean package
